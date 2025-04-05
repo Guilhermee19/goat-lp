699248ad54e11e6b57df89fd24cf6d3379ccf835
Guilhermee19 <guilherme@noclaftech.com>
2025-04-05 00:30:08 -0300

    feat: adicionar componente WhatDrivesUs e imagem navegador-light à página Sobre Nós

--- src/app/(public)/about-us/page.tsx
+++ src/app/(public)/about-us/page.tsx
@@ -4,11 +4,13 @@ import { Footer } from '@/components';
 import { CardCreateAccount, StayInTouch } from '@/components/sections';
 // import { VideoHeader } from './components/video-header';
 import { WhyGoatcom } from './components/why-goatcom';
+import { WhatDrivesUs } from './components/what-drives-us';
 
 const AboutUs = () => {
   return (
     <>
       {/* <VideoHeader></VideoHeader> */}
+      <WhatDrivesUs></WhatDrivesUs>
       <WhyGoatcom></WhyGoatcom>
       <CardCreateAccount
         title="Estamos aqui para transformar seu negócio."
